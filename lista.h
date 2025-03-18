fa28d6cb27b94f1da06fce8557bb9f826d1bf299
Brayan <btellez948@gmail.com>
2025-03-18 16:09:19 -0600

    Comentando

--- lista.h
+++ lista.h
@@ -7,21 +7,44 @@
  * de una lista doblemenete enlazada.
  */
 typedef struct _nodo_t Nodo;
+
 /**
  * Definimos un puntero genérico para nuestra lista, así nos aseguramos de
- * abstraer el comportamiento de la lista, dependerá de 
+ * abstraer el comportamiento de la lista, dependerá de la implementación
+ * y no de la estructura lista en sí.
  */
 typedef void* Valor;
 
+/**
+ * Se define un tipo genérico para comparar valores genéricos.
+ */
 typedef bool (*FuncionComparar)(Valor, Valor);
+
+/**
+ * Se define un tipo de función genérico para eliminar un valor dado.
+ */
 typedef void (*FuncionEliminar)(Valor);
+
+/**
+ * Se define un tipo de función para transformar a cadena genérico.
+ */
 typedef char* (*FuncionACadena)(Valor);
 
+/**
+ * Define una estructura completa del nodo para una lista dinámica doblemente
+ * ligada y genérico, es decir guarda un puntero, pero el nodo desconoce como
+ * manejarlo, depende de la implementación.
+ */
 struct _nodo_t {
     Nodo *siguiente, *anterior;
     Valor *valor;
 };
 
+/**
+ * Define una lista dinámica doblemente ligada y genérica, almacena el tamaño
+ * actual de la lista, el nodo inicial y final, así como las referencias a las
+ * funciones genéricas que interpretarán los nodos.
+ */
 typedef struct _lista_t {
     int tamano;
     Nodo *inicio, *final;
@@ -30,15 +53,52 @@ typedef struct _lista_t {
     FuncionACadena toString;
 } Lista;
 
+/**
+ * Permite crear un nuevo nodo, es decir, reserva la memoria para el nodo e
+ * inicializa cada una de sus propiedades.
+ */
 Nodo *nuevo_nodo(Nodo*, Nodo*, Valor);
+/**
+ * Función por defecto usada cuando no se crea una lista con una función de
+ * comparación, simplemente compara las direcciones de los nodos, es preventiva.
+ */
 bool nodo_comparar_direccion(Valor, Valor);
+/**
+ * Permite limpiar la memoria para el nodo en cuestión una vez que haya terminado
+ * de usarse el mismo, es útil porque evita dejar basura en memoria.
+ */
 void nodo_eliminar(Nodo*, FuncionEliminar);
+/**
+ * Se usa para convertir un nodo a una cadena, se uso principalmente para depurar
+ * y probar el funcionamiento de la lista.
+ */
 char *nodo_a_cadena(Nodo*, FuncionACadena);
 
+/**
+ * Permite crear una nueva lista, es decir reservar la memoria e inicializar cada
+ * una de las propiedades de la misma, así como sus métodos genéricos.
+ */
 Lista *nueva_lista(FuncionComparar, FuncionEliminar, FuncionACadena);
+/**
+ * Permite agregar un nuevo valor dentro de la lista, para ello hace uso de las
+ * funciones de comparación designadas, en pro de evitar repeticiones (si así
+ * lo indica la función de comparación).
+ */
 int lista_insertar(Lista*, Valor);
+/**
+ * Busca la primera coincidencia de un valor dentro de la lista, a través de la
+ * función de comparación asignada.
+ */
 int lista_buscar(Lista, Valor);
+/**
+ * Permite eliminar la lista, evitando que se quede basura dentro de la memoria y
+ * liberando el espacio para usarlo a posterior.
+ */
 void lista_eliminar(Lista*);
+/**
+ * Permite imprimir la lista de manera detallada, se usa para poder realizar las
+ * pruebas del funcionamiento de las listas doblemente ligadas.
+ */
 void lista_imprimir_detalles(Lista*);
 
 #endif // _PROYECTO_COMPILADORES_LISTA
--- token.h
+++ token.h
@@ -6,6 +6,11 @@
 
 #define REGISTRO_CADENA_LONGITUD    100
 
+/**
+ * Definimos una enumeración que contendrá los tipos de datos que
+ * serán aceptados dentro de nuestro lenguaje, estos están asociados
+ * a los tipos de las variables.
+ */
 typedef enum _tipo_dato_t {
     BOLEANO,
     CADENA,
@@ -14,6 +19,10 @@ typedef enum _tipo_dato_t {
     SIN_TIPO
 } TipoDato;
 
+/**
+ * Define los tipos de tablas que tendremos en nuestro lenguaje, uno por
+ * cada clase de componentes léxicos y otro para los tokens.
+ */
 typedef enum _tabla_tipo_t {
     TOKEN,
     RESERVADA,
@@ -27,11 +36,18 @@ typedef enum _tabla_tipo_t {
     LITERAL_REAL,
 } TablaTipo;
 
+/**
+ * Define la estructura para un tipo identificador, estos deben tener un
+ * nombre y un tipo asociado.
+ */
 typedef struct _identificador_t {
     char *identificador;
     TipoDato tipo;
 } Identificador;
 
+/**
+ * Define los posibles valores que podrá almacenar una de nuestras tablas.
+ */
 typedef union _valor_token_t {
     // Para identificador
     Identificador *v_identificador;
@@ -43,10 +59,19 @@ typedef union _valor_token_t {
     float *v_real;
 } ValorTabla;
 
+/**
+ * Define la estructura de cada registro de nuestra tabla de tokens,
+ * contiene una clase, es decir a quien pertenece el componente léxico
+ * y una posición dentro de la tabla en cuestión.
+ */
 typedef struct _token_t {
     int clase, posicion;
 } Token;
 
+/**
+ * Permite crear cada uno de los registros de nuestra tabla, corresponde
+ * a los valores guardados dentro de los nodos de la lista.
+ */
 typedef struct _registro_tabla_t {
     int indice;
     TablaTipo tipo;
@@ -57,31 +82,99 @@ typedef struct _registro_tabla_t {
     };
 } RegistroTabla;
 
+/**
+ * Permite trabajar con una tabla de componentes léxicos y tokens, debe especificar
+ * un nombre, el tipo de dato que almacenará y la lista que contendrá la información.
+ */
 typedef struct _tabla_t {
     char *nombre;
     TablaTipo tablaTipo;
     Lista *lista;
 } Tabla;
 
+/**
+ * Transforma las cadenas de entrada en valores enteros de salida, para ello normaliza
+ * los números de la versión del lenguaje a la versión estándar.
+ */
 int *nuevo_entero(const char*);
+
+/**
+ * Transforma las cadenas de entrada en valores reales de salida, para ello normaliza
+ * los números de la versión del lenguaje a la versión estándar internacional, es decir
+ * separados por puntos.
+ */
 float *nuevo_real(const char*);
+
+/**
+ * Clona una cadena a una nueva dirección de memoria, reserva la memoria y guarda la
+ * información de la anterior cadena a la nueva.
+ */
 char *nueva_cadena(const char*);
+
+/**
+ * Permite crear un nuevo identificador a partir de un nombre que lo identifique o símbolo
+ * y un tipo de dato asociado.
+ */
 Identificador *nuevo_identificador(const char *, TipoDato);
+/**
+ * Permtie limpiar la memoria de un valor de una tabla.
+ */
 void valor_tabla_eliminar(ValorTabla*, TablaTipo);
 
+/**
+ * Permite crear un nuevo token, reserva la memoria e inicializa los valores que debe tener
+ * el token, como la clase y posición.
+ */
 Token *nuevo_token(int, int);
+/**
+ * Usada para poder eliminar la información de un token, es decir elimina la basura y
+ * libera el espacio usado.
+ */
 void token_eliminar(Token*);
 
+/**
+ * Permite crear un nuevo registro de tablas, para ello necesita saber su índice dentro de
+ * la tabla, el tipo de dato que le corresponde y su valor.
+ */
 RegistroTabla *nuevo_registro_tabla(int, TablaTipo, Valor);
+/**
+ * Usada para comparar los valores dentro de una tabla según el tipo de dato que guarda.
+ */
 bool registro_tabla_comparar(Valor, Valor);
+/**
+ * Permite limpiar la memoria y eliminar la basura que estaba siendo usada por el
+ * registro de la tabla.
+ */
 void registro_tabla_eliminar(RegistroTabla*, TablaTipo);
+/**
+ * Permite transformar un registro a su versión de cadena.
+ */
 char *registro_a_cadena(RegistroTabla*, TablaTipo);
 
+/**
+ * Permite que se cree una tabla con un nombre y para un tipo de datos concretos.
+ */
 Tabla *nueva_tabla(const char*, TablaTipo);
+/**
+ * Permite rellenar una tabla a partir de un conjunto de argumentos variables.
+ */
 void tabla_llenar(Tabla*, int, ...);
+/**
+ * Permite la inserción de un elemento dentro e muestra tabla.
+ */
 int tabla_insertar(Tabla*, Valor);
+/**
+ * Permite realizar una búsqueda simple dentro de la lista.
+ */
 int tabla_buscar(Tabla, Valor);
+/**
+ * Permite limpiar la memoria y eliminar la basura que estaba siendo usada por nuestra tabla.
+ */
 void tabla_eliminar(Tabla*);
+/**
+ * Permite limpiar la memoria y eliminar la basura que estaba siendo usada por el
+ * Rusia
+ */
 void tabla_imprimir(FILE*, Tabla);
 
 #endif // _PROYECTO_COMPILADORES_TOKEN
